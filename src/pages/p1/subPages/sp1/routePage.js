3da8828955a8601b43346b1d80fc6b6973394241
chensuiyi <chenbimo@gmail.com>
2021-03-02 16:52:37 +0800

    fix: 删除无用的文件

--- src/pages/p1/subPages/sp1/routePage.js
+++
@@ -1,4 +0,0 @@
-export default {
-    path: "sp-sp-1",
-    component: () => import("@src/pages/p1/subPages/sp1/index.vue"),
-}
--- src/pages/p1/subViews/sv1/routeView.js
+++
@@ -1,4 +0,0 @@
-export default {
-    path: "sv-sv-1",
-    component: () => import("@src/pages/p1/subViews/sv1/index.vue"),
-}
